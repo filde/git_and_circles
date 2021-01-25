3ea840772761b201d4de5a9153d79748c98a1d1b
filde <dmtr.filimonov@yandex.ru>
2021-01-25 22:53:25 +0300

    second

--- Git и окружности.py
+++ Git и окружности.py
@@ -1,25 +1,36 @@
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtCore import Qt, QPoint, QPointF
-from PyQt5 import uic
 from random import randint
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Example(QWidget):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(300, 400)
+        self.btn = QtWidgets.QPushButton(Form)
+        self.btn.setGeometry(QtCore.QRect(50, 320, 200, 50))
+        self.btn.setObjectName("btn")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Git и жёлтые окружности"))
+        self.btn.setText(_translate("Form", "Нарисовать окружность"))
+
+
+class Example(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.w = 300
-        self.h = 300
-        uic.loadUi('UI.ui', self)
+        self.w = self.h = 300
+        self.setupUi(self)
         self.btn.clicked.connect(self.run)
         self.do_paint = False
-        self.setMouseTracking(True)
-
 
     def run(self):
         self.do_paint = True
@@ -32,7 +43,7 @@ class Example(QWidget):
         qp.begin(self)
         x, y = randint(5, self.w - 5), randint(5, self.h - 5)
         r = randint(4, min(self.w - x, x, y, self.h - y))
-        qp.setBrush(QColor('yellow'))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         qp.drawEllipse(QPoint(x, y), r, r)
         qp.end()
         self.do_paint = False
